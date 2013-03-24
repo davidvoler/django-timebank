31cdcc289fe266601c22d68043d99a97d5e12a14
davidvoler <davidvoler@gmail.com>
2013-03-24 07:42:30 +0100

    ading static

--- project/urls.py
+++ project/urls.py
@@ -1,4 +1,6 @@
 from django.conf.urls import patterns, include, url
+from django.conf import settings
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
@@ -19,3 +21,10 @@ urlpatterns = patterns('',
     url(r'^accounts/', include('timebank.accounts.urls')),
     #url(r'^reservations/', include('timebank.reservations.urls')),
 )
+urlpatterns += staticfiles_urlpatterns()
+
+if settings.DEBUG:
+    urlpatterns += patterns('',
+        (r'^media/(?P<path>.*)$', 'django.views.static.serve',
+         {'document_root': settings.MEDIA_ROOT}),
+    )
